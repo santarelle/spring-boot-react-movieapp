cf27fc7dda162efeac2a9df9147c7943e1a739de
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2022-05-11 18:30:13 -0400

    feat: initial commit

---
+++ backend/src/test/java/com/santarelle/movie/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santarelle.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
